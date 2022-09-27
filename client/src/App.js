0d8e75ea370accd6c7ea5919d6a72ed7a44dc6ba
samyyuv <samyy.uv@gmail.com>
2022-09-27 15:09:38 +0200

    doing the structure of home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import bug from './images/bug.png'
+import useStyles from './styles'
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Bug Manager</Typography>
+        <img className={classes.image} src={bug} alt="bug" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
