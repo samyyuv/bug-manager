0fc18e9930d91e25f8d138a3a8fef22f90054daa
samyyuv <samyy.uv@gmail.com>
2022-10-03 12:56:23 +0200

    fixing tags and likes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -38,11 +38,13 @@ const Post = ({ post, setCurrentId }) => {
       </CardContent>
       <CardActions>
         <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
-          <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}
+          <ThumbUpAltIcon fontSize="small" />
+          &nbsp; Like &nbsp;
+          {post.likeCount}
         </Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small" />
-          Delete
+          &nbsp; Delete
         </Button>
       </CardActions>
     </Card>
