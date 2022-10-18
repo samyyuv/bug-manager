685aa87e54a1060884ad4f1a63669c95ff82da5d
samyyuv <samyy.uv@gmail.com>
2022-10-18 18:04:26 +0200

    finishing recommended posts feature

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -36,21 +36,21 @@ const Post = ({ post, setCurrentId }) => {
 
   return (
     <Card className={classes.card} raised elevation={6}>
+      {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+        <div className={classes.overlay2}>
+          <Button
+            style={{ color: 'white' }}
+            size="small"
+            onClick={() => setCurrentId(post._id)}>
+            <MoreHorizIcon fontSize="medium" />
+          </Button>
+        </div>)}
       <ButtonBase className={classes.cardAction} onClick={openPost}>
         <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
         <div className={classes.overlay}>
           <Typography variant="h6">{post.name}</Typography>
           <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
-        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
-          <div className={classes.overlay2}>
-            <Button
-              style={{ color: 'white' }}
-              size="small"
-              onClick={() => setCurrentId(post._id)}>
-              <MoreHorizIcon fontSize="medium" />
-            </Button>
-          </div>)}
         <div className={classes.details}>
           <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)} </Typography>
         </div>
